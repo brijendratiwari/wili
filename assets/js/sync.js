76be82e9f61166ab652eaf7d7d126e02a8d8e4a2
sandeep <sandeep@ignisitsolutions.com>
2014-10-20 16:35:59 +0530

    changes

--- assets/js/sync.js
+++ assets/js/sync.js
@@ -37,6 +37,28 @@ function stopsync() {
 
 }
 
+function stopallsync(id){
+ var base_url = $('#base_url').val();
+     $.ajax({
+        url: base_url + "sync/StopSync",
+        type: "POST",
+        data: {sync: id},
+        beforeSend: function() {
+             var n = noty({layout: 'topCenter', type: 'warning', text: 'Please wait', timeout: 5000});
+            if(id == 1){
+                $('#et_stopsync').addClass('disabled');
+            }
+            if(id==2){
+                $('#bb_stopsync').addClass('disabled');
+            }
+        }
+    }).done(function(msg) {
+         
+    }).fail(function(jqXHR, textStatus) {
+         
+    });
+}
+
 function startsync(id) {
 
     var base_url = $('#base_url').val();
@@ -65,23 +87,42 @@ function startsync(id) {
 
         }
     }).done(function(msg) {
+        if (msg != 'stop')
+        {
+            var data = JSON.parse(msg);
+            $('#et_subscribe').text(data.SubscribedCount);
+            $('#et_unsubscribe').text(data.UnSubscribedCount);
+            $('#et_lastsync').text(data.SyncTime);
 
-        var data = JSON.parse(msg);
-        $('#et_subscribe').text(data.SubscribedCount);
-        $('#et_unsubscribe').text(data.UnSubscribedCount);
-        $('#et_lastsync').text(data.SyncTime);
+            var n = noty({layout: 'topCenter', type: 'information', text: 'Manual Sync Successfully', timeout: 2000});
 
-        var n = noty({layout: 'topCenter', type: 'information', text: 'Manual Sync Successfully', timeout: 2000});
-
+            $('#et_startsync').removeClass('disabled');
+            $('#et_stopsync').addClass('disabled');
+            clearInterval(stopper);
+            $(document).find("#exact_progessbar .progress-bar").attr("style", "width: 100%");
+            $("#exact_progessbar").siblings('.progress-stat-value').html('100%');
+            $("#exact_progessbar span").html('100%');
+            $("#exact_progessbar").parent('.progress-stat').addClass('hide');
+        }
+        else {
+            var n = noty({layout: 'topCenter', type: 'error', text: 'Manual Sync Stoped', timeout: 2000});
+            $('#et_startsync').removeClass('disabled');
+            $('#et_stopsync').addClass('disabled');
+            clearInterval(stopper);
+        $(document).find("#exact_progessbar .progress-bar").attr("style", "width: 0%");
+            $("#exact_progessbar").siblings('.progress-stat-value').html('0%');
+            $("#exact_progessbar span").html('0%');
+            $("#exact_progessbar").parent('.progress-stat').addClass('hide');
+        }
+    }).fail(function(jqXHR, textStatus) {
+        var n = noty({layout: 'topCenter', type: 'error', text: 'Manual Sync Stoped', timeout: 2000});
         $('#et_startsync').removeClass('disabled');
         $('#et_stopsync').addClass('disabled');
         clearInterval(stopper);
-        $(document).find("#exact_progessbar .progress-bar").attr("style", "width: 100%");
-        $("#exact_progessbar").siblings('.progress-stat-value').html('100%');
-        $("#exact_progessbar span").html('100%');
-        $("#exact_progessbar").parent('.progress-stat').addClass('hide');
-    }).fail(function(jqXHR, textStatus) {
-        $('#et_startsync').removeClass('disabled');
+        $(document).find("#exact_progessbar .progress-bar").attr("style", "width: 0%");
+            $("#exact_progessbar").siblings('.progress-stat-value').html('0%');
+            $("#exact_progessbar span").html('0%');
+            $("#exact_progessbar").parent('.progress-stat').addClass('hide');
     });
 }
 
@@ -113,40 +154,58 @@ function startblackboxxsync(id) {
 
         }
     }).done(function(msg) {
+        if (msg != 'stop')
+        {
+            var data = JSON.parse(msg);
+            $('#bb_subscribe').text(data.SubscribedCount);
+            $('#bb_unsubscribe').text(data.UnSubscribedCount);
+            $('#bb_lastsync').text(data.SyncTime);
 
-        var data = JSON.parse(msg);
-        $('#bb_subscribe').text(data.SubscribedCount);
-        $('#bb_unsubscribe').text(data.UnSubscribedCount);
-        $('#bb_lastsync').text(data.SyncTime);
+            var n = noty({layout: 'topCenter', type: 'information', text: 'Manual Sync Successfully', timeout: 2000});
 
-        var n = noty({layout: 'topCenter', type: 'information', text: 'Manual Sync Successfully', timeout: 2000});
-
+            $('#bb_startsync').removeClass('disabled');
+            $('#bb_stopsync').addClass('disabled');
+            clearInterval(stopper);
+            $(document).find("#bb_progessbar .progress-bar").attr("style", "width: 100%");
+            $("#bb_progessbar").siblings('.progress-stat-value').html('100%');
+            $("#bb_progessbar span").html('100%');
+            $("#bb_progessbar").parent('.progress-stat').addClass('hide');
+        }
+         else {
+            var n = noty({layout: 'topCenter', type: 'error', text: 'Manual Sync Stoped', timeout: 2000});
+            $('#bb_startsync').removeClass('disabled');
+            $('#bb_stopsync').addClass('disabled');
+            clearInterval(stopper);
+            $(document).find("#bb_progessbar .progress-bar").attr("style", "width: 0%");
+            $("#bb_progessbar").siblings('.progress-stat-value').html('0%');
+            $("#bb_progessbar span").html('0%');
+            $("#bb_progessbar").parent('.progress-stat').addClass('hide');
+        }
+    }).fail(function(jqXHR, textStatus) {
         $('#bb_startsync').removeClass('disabled');
         $('#bb_stopsync').addClass('disabled');
-        clearInterval(stopper);
-        $(document).find("#bb_progessbar .progress-bar").attr("style", "width: 100%");
-        $("#bb_progessbar").siblings('.progress-stat-value').html('100%');
-        $("#bb_progessbar span").html('100%');
-        $("#bb_progessbar").parent('.progress-stat').addClass('hide');
-    }).fail(function(jqXHR, textStatus) {
-        $('#et_startsync').removeClass('disabled');
+         clearInterval(stopper);
+            $(document).find("#bb_progessbar .progress-bar").attr("style", "width: 0%");
+            $("#bb_progessbar").siblings('.progress-stat-value').html('0%');
+            $("#bb_progessbar span").html('0%');
+            $("#bb_progessbar").parent('.progress-stat').addClass('hide');
     });
 }
 
-$(document).ready(function() {
-    var countdown = $("#auto_sync_time").val() * 60 * 1000;
-    var timerId = setInterval(function() {
-        countdown -= 1000;
-        var min = Math.floor(countdown / (60 * 1000));
-        //var sec = Math.floor(countdown - (min * 60 * 1000));  // wrong
-        var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000);  //correct
-
-        if (countdown <= 0) {
-            clearInterval(timerId);
-            //doSomething();
-        } else {
-            $("#exact_target_timer").text(min + "Min : " + sec+"Secs");
-        }
-
-    }, 1000);
-})
+//$(document).ready(function() {
+//    var countdown = $("#auto_sync_time").val() * 60 * 1000;
+//    var timerId = setInterval(function() {
+//        countdown -= 1000;
+//        var min = Math.floor(countdown / (60 * 1000));
+//        //var sec = Math.floor(countdown - (min * 60 * 1000));  // wrong
+//        var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000);  //correct
+//
+//        if (countdown <= 0) {
+//            clearInterval(timerId);
+//            //doSomething();
+//        } else {
+//            $("#exact_target_timer").text(min + "Min : " + sec + "Secs");
+//        }
+//
+//    }, 1000);
+//})
